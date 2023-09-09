bfc0fccd715528957a251952c82f0d809894b537
Kevin Perez <kevper2019@gmail.com>
2023-09-09 00:14:44 -0400

    Populate board and fill-in functionalities

---
+++ script.js
@@ -0,0 +1,32 @@
+const board = document.querySelector('.board')
+
+
+let numberOfSquares = 16;
+let boardSize = 600;
+
+for (let i = 0; i < numberOfSquares; i++) {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    board.appendChild(row);
+    
+    for (let i = 0; i < numberOfSquares; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${boardSize/numberOfSquares}px`;
+        square.style.height = `${boardSize/numberOfSquares}px`;
+        row.appendChild(square);
+    }
+}
+
+
+function fillSquare() {
+
+}
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach( square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "black";
+    });
+});
