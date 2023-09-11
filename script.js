61a1a937a283f0bd029f88f5ef7b143d64daef9a
Kevin Perez <kevper2019@gmail.com>
2023-09-11 17:38:11 -0400

    Add change size, clear buttons

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 const board = document.querySelector('.board')
 
+let boardSize = 700;
 
-let numberOfSquares = 65;
-let boardSize = 600;
-
-for (let i = 0; i < numberOfSquares; i++) {
-    let row = document.createElement('div');
-    row.classList.add('row');
-    board.appendChild(row);
-    
+//Initializes board of given size
+function initBoard(numberOfSquares) {
     for (let i = 0; i < numberOfSquares; i++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.style.width = `${boardSize/numberOfSquares}px`;
-        square.style.height = `${boardSize/numberOfSquares}px`;
-        row.appendChild(square);
+        let row = document.createElement('div');
+        row.classList.add('row');
+        board.appendChild(row);
+        
+        for (let i = 0; i < numberOfSquares; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `${boardSize/numberOfSquares}px`;
+            square.style.height = `${boardSize/numberOfSquares}px`;
+            row.appendChild(square);
+        }
     }
 }
 
 
-const squares = document.querySelectorAll('.square');
+initBoard(16);
 
-squares.forEach( square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "black";
+//Add event listener to fill in squares
+let squares = document.querySelectorAll('.square');
+function colorListener() {
+    squares.forEach( square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        });
+    });
+}
+colorListener();
+
+
+
+//Clears board
+document.getElementById('clear').addEventListener('click', () => {
+    squares.forEach( square => {
+        square.style.backgroundColor = "white";
     });
 });
+
+const slider = document.querySelector('.slider');
+const barSize = document.querySelector('.bar-size')
+barSize.innerHTML = slider.value;
+
+slider.oninput = () => {
+    barSize.innerHTML = slider.value;
+}
+
+document.querySelector(".change").addEventListener("click", () => {
+    board.innerHTML = "";
+    initBoard(slider.value);
+    squares = document.querySelectorAll('.square');
+    colorListener();
+})
